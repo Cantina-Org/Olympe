521eb73939668c22b2d8c8b8eb9cf4d479cc6dfb
matyu <degueurce.mathieu@gmail.com>
2023-04-23 19:20:05 +0200

    - Import et modif de la partie admin de Nephelees

---
+++ Cogs/admin/add_user.py
@@ -0,0 +1,52 @@
+from hashlib import new
+from os import mkdir
+from uuid import uuid3, uuid1
+from flask import render_template, url_for
+from werkzeug.utils import secure_filename, redirect
+from Utils.utils import user_login, make_log
+from argon2 import PasswordHasher
+
+ph = PasswordHasher()
+dir_path = '/home/cantina/nephelees/file_cloud'
+share_path = '/home/cantina/nephelees/share'
+
+
+def add_user_cogs(ctx, database):
+    admin = 0
+    admin_and_login = user_login(database, ctx)
+    if admin_and_login[0] and admin_and_login[1]:
+        if ctx.method == 'GET':
+            user_name = database.select('''SELECT user_name FROM cantina_administration.user WHERE token=%s''',
+                                        (ctx.cookies.get('userID'),))
+            return render_template('admin/add_user.html', user_name=user_name)
+        elif ctx.method == 'POST':
+
+            if ctx.form['pword1'] == ctx.form['pword2']:
+                data = ctx.form
+                new_uuid = str(uuid3(uuid1(), str(uuid1())))
+                new_salt = new('sha256').hexdigest()
+                try:
+                    for i in data:
+                        if i == 'admin':
+                            admin = 1
+                        else:
+                            pass
+
+                    database.insert('''INSERT INTO cantina_administration.user(token, user_name, salt, password, 
+                            admin, work_Dir) VALUES (%s, %s, %s, %s, %s, %s)''',
+                                    (new_uuid, ctx.form['uname'], new_salt,
+                                     ph.hash(ctx.form['pword2']),
+                                     admin,
+                                     dir_path + '/' + secure_filename(ctx.form['uname'])))
+
+                except Exception as error:
+                    make_log('Error', ctx.remote_addr, ctx.cookies.get('userID'), 2, str(error))
+
+                mkdir(dir_path + '/' + secure_filename(ctx.form['uname']))
+                mkdir(share_path + '/' + secure_filename(ctx.form['uname']))
+                make_log('add_user', ctx.remote_addr, ctx.cookies.get('userID'), 2,
+                         'Created user token: ' + new_uuid)
+                return redirect(url_for('admin_show_user'))
+    else:
+        make_log('Error', ctx.remote_addr, ctx.cookies.get('userID'), 2, database)
+        return redirect(url_for('home'))
--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from json import load
 from os import path, getcwd
 from flask import Flask, request
+
+from Cogs.admin.add_user import add_user_cogs
+from Cogs.admin.home import home_admin_cogs
+from Cogs.admin.show_log import show_log_cogs
+from Cogs.admin.show_user import show_user_cogs
 from Cogs.home import home_cogs
 from Cogs.login import login_cogs
 from Cogs.my_account import my_account_cogs
@@ -29,6 +34,32 @@ def my_account():
 @app.route('/login')
 def auth():
     return login_cogs(request, database)
+
+
+# Fonction permettant de voire la page 'principale' du panel Admin de Cantina Cloud
+@app.route('/admin/home')
+def admin_home():
+    return home_admin_cogs(request, database)
+
+
+# Fonction permettant de visualiser les utilisateur de Cantina Cloud
+@app.route('/admin/usermanager/')
+@app.route('/admin/usermanager/<user_name>')
+def admin_show_user(user_name=None):
+    return show_user_cogs(request, database, user_name)
+
+
+# Fonction permettant de créer un utilisateur
+@app.route('/admin/add_user/', methods=['POST', 'GET'])
+def admin_add_user():
+    return add_user_cogs(request, database)
+
+
+# Fonction permettant de voire les logs générer par Cantina Cloud
+@app.route('/admin/show_log/')
+@app.route('/admin/show_log/<log_id>')
+def admin_show_log(log_id=None):
+    return show_log_cogs(request, database, log_id)
 
 
 if __name__ == '__main__':
