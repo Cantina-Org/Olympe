8b48f1f58fc63f9c2d64e8085c17368726a97951
matyu <degueurce.mathieu@gmail.com>
2023-08-14 01:43:30 +0200

    - http:// -> https:// : login.py

--- Cogs/login.py
+++ Cogs/login.py
@@ -4,4 +4,4 @@ from flask import redirect
 def login_cogs(ctx, database):
     to_redirect = database.select('''SELECT fqdn FROM cantina_administration.domain WHERE name="cerbere"''',
                                   number_of_data=1)
-    return redirect('http://'+to_redirect[0]+'/auth/nephelees', code=302)
+    return redirect('https://'+to_redirect[0]+'/auth/nephelees', code=302)
