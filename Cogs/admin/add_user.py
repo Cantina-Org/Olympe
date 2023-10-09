20e21b32e05d2beb0d943b18ebfc7e01f816afb3
mathieu <degueurce.mathieu@gmail.com>
2023-10-09 22:27:45 +0200

    Des changements dans tout les sens pour que ça remarche

--- Cogs/admin/add_user.py
+++ Cogs/admin/add_user.py
@@ -12,10 +12,6 @@ ph = PasswordHasher()
 def add_user_cogs(ctx, database):
     admin = 0
     admin_and_login = user_login(database, ctx)
-    dir_path = database.select("""SELECT content FROM cantina_administration.config WHERE name = %s""",
-                               ("dir_path",), 1)[0]
-    share_path = database.select("""SELECT content FROM cantina_administration.config WHERE name = %s""",
-                                 ("share_path",), 1)[0]
 
     if admin_and_login[0] and admin_and_login[1]:
         if ctx.method == 'GET':
@@ -29,22 +25,38 @@ def add_user_cogs(ctx, database):
                 new_uuid = str(uuid3(uuid1(), str(uuid1())))
                 new_salt = new('sha256').hexdigest()
                 try:
+                    dir_path = database.select("""SELECT content FROM cantina_administration.config WHERE name = %s""",
+                                                ("dir_path",), 1)[0]
+                except TypeError:
+                    dir_path = None
+                try:
+                    share_path = database.select("""SELECT content FROM cantina_administration.config WHERE name = %s""",
+                                                 ("share_path",), 1)[0]
+                except TypeError:
+                    share_path = None
+                try:
                     for i in data:
                         if i == 'admin':
                             admin = 1
-
+                    if dir_path:
+                        work_path = dir_path + '/' + secure_filename(ctx.form['uname'])
+                    else:
+                        work_path = None
                     database.insert('''INSERT INTO cantina_administration.user(token, user_name, salt, password, 
                             admin, work_Dir) VALUES (%s, %s, %s, %s, %s, %s)''',
                                     (new_uuid, ctx.form['uname'], new_salt,
                                      ph.hash(ctx.form['pword2']),
-                                     admin,
-                                     dir_path + '/' + secure_filename(ctx.form['uname'])))
+                                     admin, work_path
+                                     ))
 
                 except Exception as error:
+                    print(error)
                     make_log('Error', ctx.remote_addr, ctx.cookies.get('userID'), 2, database, str(error))
+                    return redirect(url_for('admin_add_user'))
 
-                mkdir(dir_path + '/' + secure_filename(ctx.form['uname']))
-                mkdir(share_path + '/' + secure_filename(ctx.form['uname']))
+                if dir_path and share_path:
+                    mkdir(dir_path + '/' + secure_filename(ctx.form['uname']))
+                    mkdir(share_path + '/' + secure_filename(ctx.form['uname']))
                 make_log('add_user', ctx.remote_addr, ctx.cookies.get('userID'), 2, database,
                          'Created user token: ' + new_uuid)
                 return redirect(url_for('admin_show_user'))
