fc04ccb0d201b5b5ef2a03b81881c877c621964a
mathieu <degueurce.mathieu@gmail.com>
2023-10-09 22:05:46 +0200

    Des changements dans tout les sens pour que ça remarche

--- Cogs/my_account.py
+++ Cogs/my_account.py
@@ -1,5 +1,6 @@
 from subprocess import check_output, CalledProcessError
 from flask import redirect, url_for, render_template
+from pymysql import err
 
 
 def my_account_cogs(ctx, database):
@@ -8,9 +9,16 @@ def my_account_cogs(ctx, database):
 
     user_data = database.select('''SELECT * FROM cantina_administration.user WHERE token=%s''',
                                 (ctx.cookies.get('token')), 1)
-    mess_data = database.select('''SELECT * FROM cantina_hermes.data WHERE token=%s''', (ctx.cookies.get('token')), 1)
+    try:
+        mess_data = database.select('''SELECT * FROM cantina_hermes.data WHERE token=%s''', (ctx.cookies.get('token')), 1)
+    except err.ProgrammingError:
+        mess_data = "error"
+
     try:
         folder_size = check_output(['du', '-sh', user_data[6]]).split()[0].decode('utf-8')
     except CalledProcessError:
         folder_size = 'error'
+    except TypeError:
+        folder_size = 'error'
+
     return render_template('my_account.html', user_data=user_data, messsage_data=mess_data, folder_size=folder_size)
