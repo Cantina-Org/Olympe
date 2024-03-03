3a2413e0a4f63a49db771a7e8bb63f9dd4eeac34
matyu <degueurce.mathieu@gmail.com>
2024-03-03 18:30:56 +0100

    début
    Took 1 hour 30 minutes

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from cantinaUtils import Database
+from os import path, getcwd
+from json import load
 
-app = Flask(__name__)
+file_path = path.abspath(path.join(getcwd(), "config.json"))  # Trouver le chemin complet du fichier config.json
+
+# Lecture du fichier JSON
+with open(file_path, 'r') as file:
+    config_data = load(file)  # Ouverture du fichier config.json
+
+
+print(config_data)
+app = Flask(__name__)  # Création de l'application Flask
+socketio = SocketIO(app)  # Lien entre l'application Flaks et le WebSocket
+
+database = Database.DataBase(
+    user=config_data['database'][0]['username'],
+    password=config_data['database'][0]['password'],
+    host=config_data['database'][0]['address'],
+    port=config_data['database'][0]['port']
+)  # Création de l'objet pour se connecter à la base de données via le module cantina
+database.connection()  # Connexion à la base de données
+
+
+database.exec("""CREATE TABLE IF NOT EXISTS cantina_administration.user(id INT PRIMARY KEY, token TEXT, 
+username TEXT, password TEXT, email TEXT, email_verified BOOL, email_verification_code TEXT, A2F BOOL, A2F_secret TEXT, 
+last_connection DATE, admin BOOL, desactivated BOOL)""", None)
 
 
 @app.route('/')
 def home():
-    return 'Hello world'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, allow_unsafe_werkzeug=True, debug=True, port=5001)
