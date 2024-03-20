b3f3c3371d369ddc2e228c46dc51917dafb0368d
matyu <degueurce.mathieu@gmail.com>
2024-03-20 12:59:04 +0100

    Début création d'user
    Took 9 minutes

--- Cogs/Administration/add_user.py
+++ Cogs/Administration/add_user.py
@@ -1,3 +1,5 @@
+from uuid import uuid3, uuid1
+from argon2 import PasswordHasher
 from Utils.verify_login import verify_login
 from flask import redirect, url_for, request, render_template
 
@@ -9,11 +11,8 @@ def add_user_cogs(database):
             return redirect(url_for('show_user'))
 
         if request.method == 'POST':
-            database.exec('''DELETE FROM cantina_administration.user WHERE token = %s''',
-                          (request.form['token_to_delete']))
-            database.exec('''DELETE FROM cantina_administration.permission WHERE user_token = %s''',
-                          (request.form['token_to_delete']))
-            return redirect(url_for('show_user'))
+            _create_user = create_user()
+            return redirect(url_for('show_user', user_token=_create_user))
         elif request.method == 'GET':
             return render_template('Administration/add_user.html')
 
@@ -21,3 +20,31 @@ def add_user_cogs(database):
         return redirect(url_for('sso_login', error='2'))
     else:
         return redirect(url_for('sso_login'))
+
+
+def create_user(database, username, password, email, admin, show_log, edit_username, edit_email,
+                edit_password, edit_profile_picture, edit_A2F, edit_ergo, show_specific_account, edit_username_admin,
+                edit_email_admin, edit_password_admin, edit_profile_picture_admin, allow_edit_username,
+                allow_edit_email, allow_edit_password, allow_edit_profile_picture, allow_edit_A2F, create_user,
+                delete_account, desactivate_account, edit_permission,  show_all_modules, on_off_modules,
+                on_off_maintenance, delete_modules, add_modules):
+
+    token = str(uuid3(uuid1(), str(uuid1())))
+    hashed_password = PasswordHasher().hash(password)
+
+    database.exec("""INSERT INTO cantina_administration.user(token, username, password, email, admin) 
+    VALUES (%s, %s, %s, %s, %s)""", (token, username, hashed_password, email, admin))
+
+    database.exec("""INSERT INTO cantina_administration.permission(user_token, show_log, edit_username, edit_email, 
+    edit_password, edit_profile_picture, edit_A2F, edit_ergo, show_specific_account, edit_username_admin, 
+    edit_email_admin, edit_password_admin, edit_profile_picture_admin, allow_edit_username, allow_edit_email, 
+    allow_edit_password, allow_edit_profile_picture, allow_edit_A2F, create_user, delete_account, desactivate_account, 
+    edit_permission, show_all_modules, on_off_modules, on_off_maintenance, delete_modules, add_modules) VALUES (%s, %s, 
+    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s)""",
+                  (token, show_log, edit_username, edit_email, edit_password, edit_profile_picture, edit_A2F, edit_ergo,
+                   show_specific_account, edit_username_admin, edit_email_admin, edit_password_admin,
+                   edit_profile_picture_admin, allow_edit_username, allow_edit_email, allow_edit_password,
+                   allow_edit_profile_picture, allow_edit_A2F, create_user, delete_account, desactivate_account,
+                   edit_permission, show_all_modules, on_off_modules, on_off_maintenance, delete_modules, add_modules))
+
+    return token
