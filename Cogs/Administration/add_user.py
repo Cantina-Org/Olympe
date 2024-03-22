e973768ee6cff7a635e2833b5e2f4f86e0d61eb0
matyu <degueurce.mathieu@gmail.com>
2024-03-22 15:37:22 +0100

    Des commentaires dans mon code :0
    Took 2 minutes

--- Cogs/Administration/add_user.py
+++ Cogs/Administration/add_user.py
@@ -4,15 +4,17 @@ from flask import redirect, url_for, request, render_template
 
 
 def add_user_cogs(database):
+    # Vérification de si l'utilisateur est bien connecté et n'a pas un compte désactivé
     if verify_login(database) and verify_login(database) != 'desactivated':
+        # Si l'utilisateur n'a pas les permissions, redirection vers la page d'accueil
         if not database.select("""SELECT create_user FROM cantina_administration.permission WHERE user_token = %s""",
                                (request.cookies.get('token')), 1)[0]:
             return redirect(url_for('show_user'))
 
-        if request.method == 'POST':
-            _create_user = create_user(database)
+        if request.method == 'POST':  # Si il fait une requete de type POST
+            _create_user = create_user(database)  # Création de l'utilisateur
             return redirect(url_for('show_user', user_token=_create_user))
-        elif request.method == 'GET':
+        elif request.method == 'GET':  # Si il fait une requete de type GET
             return render_template('Administration/add_user.html')
 
     elif verify_login(database) == "desactivated":
