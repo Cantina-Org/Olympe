c7655007d2814c61da38385fd31f13a42e4a5b2a
matyu <degueurce.mathieu@gmail.com>
2024-04-25 19:22:56 +0200

    début installation module
    Took 19 minutes

--- Utils/Administration/Modules/install_module.py
+++ Utils/Administration/Modules/install_module.py
@@ -1,3 +1,6 @@
+import json
+
+
 def install_module(config_file: dict):
     """
     :param config_file: list
@@ -7,8 +10,17 @@ def install_module(config_file: dict):
         exit('config_file: type error')
 
     # Vérification de si tout les éléments sont requis sont présent
+    check_if_all_items_are_presents(config_file)
 
     return 0
 
 
-print(install_module({'name': 'eee'}))
+# Fonction permettant de vérifier si tout les paramètres sont présent dans le fichier de configuration
+def check_if_all_items_are_presents(config_file: dict):
+    pass
+
+
+f = open('/Users/matyu/Code/Cantina/Olympe/Example/example-installation.json')
+data = json.load(f)
+
+print(install_module(data))
