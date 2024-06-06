99d5e7e3b716894ce2b8d6fb32e20b01122e0839
matyu <degueurce.mathieu@gmail.com>
2024-06-06 21:43:49 +0200

    Get de la valeur
    Took 18 minutes

--- static/JS/Administration/Modules/create_form.js
+++ static/JS/Administration/Modules/create_form.js
@@ -1,23 +1,23 @@
-var config = 'ee';
+let config;
 
 function create_form() {
     let files = document.getElementById('file-input').files;
     let modules_name = document.getElementById('modules-name');
 
-    if (files.length !== 0 && modules_name.value === "Autre") {
+    if (files.length !== 0 && modules_name.value === "autre") {
         let fr = new FileReader();
 
         fr.onload = async function(e) {
             console.log(e);
             let result = JSON.parse(e.target.result);
             config = JSON.stringify(result, null, 2);
-            console.log(config)
+            // console.log(config)
         }
         fr.readAsText(files.item(0));
         console.log(config)
     } else if (files.length === 0 && modules_name.value === "Autre") {
         alert('Problem in file maybe file isn\'t uploaded to webpage')
-    } else if (modules_name.value !== "Autre") {
+    } else if (modules_name.value !== "autre") {
         fetch(`https://matyu.fr/cantina/Olympe/${modules_name.value.toLowerCase()}-install-file.json`, {
             method: 'GET',
             mode: "cors",
--- static/JS/Administration/Modules/show_file_input.js
+++ static/JS/Administration/Modules/show_file_input.js
@@ -1,4 +1,6 @@
 function show_file_input(){
     let input = document.getElementById('modules-name')
-    document.getElementById('modules-name-file').hidden = input.value !== 'Autre';
+    document.getElementById('modules-name-file').hidden = input.value !== 'autre';
+    console.log(input.value !== 'autre')
+    console.log(input.value)
 }
