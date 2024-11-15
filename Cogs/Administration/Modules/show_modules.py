eabf2cf17848d672ab68838e74aca06ac37c059c
Matyu9 <59774749+Matyu9@users.noreply.github.com>
2024-11-15 10:10:48 +0100

    - Front page de modules
    Took 34 minutes

--- Cogs/Administration/Modules/show_modules.py
+++ Cogs/Administration/Modules/show_modules.py
@@ -6,6 +6,14 @@ def show_modules_cogs(database):
     if verify_login(database) and verify_login(database) != 'desactivated':
         user_permission = database.select('''SELECT * from cantina_administration.permission WHERE user_token = %s''',
                                           (request.cookies.get('token')), 1)
+
+        # On récupère les modules afin de pouvoir faire une redirection sur la page via la sidebar
+        modules_info = database.select("""SELECT * FROM cantina_administration.modules""", None)
+
+        # On récupère le thème de l'utilisateur afin de pouvoir l'afficher
+        local_user_theme = database.select('''SELECT theme FROM cantina_administration.user WHERE token= %s''',
+                                           (request.cookies.get('token')), 1)
+
         if not user_permission[23]:  # Si l'utilisateur n'as pas la permission, redirection vers la page d'accueil
             return redirect(url_for('home'))
 
@@ -17,14 +25,16 @@ def show_modules_cogs(database):
             return redirect(url_for('show_modules', module_name=request.form["module_name"]))
         else:
             if request.args.get('module_name'):
-                module_info = database.select('''SELECT * FROM cantina_administration.modules WHERE name = %s''',
+                selected_module_info = database.select('''SELECT * FROM cantina_administration.modules WHERE name = %s''',
                                               (request.args.get('module_name')), 1)
 
-                return render_template('Administration/show_one_modules.html', modules_info=module_info,
-                                       user_permission=user_permission)
+                return render_template('Administration/show_one_modules.html',
+                                       selected_module_info=selected_module_info, modules_info=modules_info,
+                                       user_permission=user_permission, local_user_theme=local_user_theme)
             else:
                 modules_info = database.select('''SELECT * FROM cantina_administration.modules''', None)
-            return render_template('Administration/show_modules.html', modules_info=modules_info)
+            return render_template('Administration/show_modules.html', modules_info=modules_info,
+                                   user_permission=user_permission, local_user_theme=local_user_theme)
     elif verify_login(database) == 'desactivated':
         return redirect(url_for('sso_login', error='2'))
     else:
