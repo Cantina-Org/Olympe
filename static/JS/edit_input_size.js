917fffe34148905744c8a139cf1c65f8dec4fd61
Matyu9 <59774749+Matyu9@users.noreply.github.com>
2024-11-21 22:44:12 +0100

    - Front edit smtp
    Took 1 hour 3 minutes

--- static/JS/edit_input_size.js
+++
@@ -1,9 +0,0 @@
-function edit_input_size(input) {
-    // Récupérer la longueur du texte
-    let longueurTexte = input.value.length;
-
-    if (longueurTexte * 10 >= input.style.width) {
-        // Ajuster la largeur de l'input en fonction de la longueur du texte
-        input.style.width = (longueurTexte * 10) + 'px'; // Vous pouvez ajuster le facteur multiplicatif selon vos besoins
-    } else {}
-}
