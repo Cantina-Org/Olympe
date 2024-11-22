ca8314d67865f031ef5dbdf743373b4966474ca6
Matyu9 <59774749+Matyu9@users.noreply.github.com>
2024-11-22 10:04:04 +0100

    - Front edit global permission
    Took 37 minutes

--- Cogs/Administration/User/global_permission.py
+++ Cogs/Administration/User/global_permission.py
@@ -5,6 +5,13 @@ from Utils.Administration.User.check_global_permission_edit import check_perm
 
 def global_permission_cogs(database):
     if verify_login(database) and verify_login(database) != 'desactivated':
+        # On récupère les modules afin de pouvoir faire une redirection sur la page via la sidebar
+        modules_info = database.select("""SELECT * FROM cantina_administration.modules""", None)
+
+        # On récupère le thème de l'utilisateur afin de pouvoir l'afficher
+        local_user_theme = database.select('''SELECT theme FROM cantina_administration.user WHERE token= %s''',
+                                           (request.cookies.get('token')), 1)
+
         permission = []
         for i in ['edit_username', 'edit_password', 'edit_email', 'edit_profile_picture', 'edit_a2f']:
             permission.append(database.select('''SELECT content FROM cantina_administration.config WHERE name = %s''',
@@ -14,6 +21,8 @@ def global_permission_cogs(database):
                                           (request.cookies.get('token')), number_of_data=1)
         if request.method == 'POST':
             for i in ['edit_username', 'edit_password', 'edit_email', 'edit_profile_picture', 'edit_a2f']:
+                print(check_perm(i))
+                print(i)
                 database.exec(f'''UPDATE cantina_administration.permission SET {i} = %s''', (check_perm(i)))
                 database.exec('''UPDATE cantina_administration.config SET content = %s WHERE name = %s''',
                               (check_perm(i), i))
@@ -24,8 +33,10 @@ def global_permission_cogs(database):
                     database.select('''SELECT content FROM cantina_administration.config WHERE name = %s''',
                                     (i,), number_of_data=1)[0])
 
+                print(permission)
+
         return render_template('Administration/global_permission.html', permission=permission,
-                               user_permission=user_permission)
+                               user_permission=user_permission, modules_info=modules_info, local_user_theme=local_user_theme)
 
     elif verify_login(database) == "desactivated":
         return redirect(url_for('sso_login', error='2'))
