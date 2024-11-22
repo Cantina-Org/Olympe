cceb9b60db08067c9667558e1979d3a8efdb71df
Matyu9 <59774749+Matyu9@users.noreply.github.com>
2024-11-22 13:52:27 +0100

    - Front create user - small fix
    Took 30 minutes

--- Cogs/Administration/User/add_user.py
+++ Cogs/Administration/User/add_user.py
@@ -11,11 +11,22 @@ def add_user_cogs(database):
                                (request.cookies.get('token')), 1)[0]:
             return redirect(url_for('show_user'))
 
+        # On récupère les modules afin de pouvoir faire une redirection sur la page via la sidebar
+        modules_info = database.select("""SELECT * FROM cantina_administration.modules""", None)
+
+        # On récupère le thème de l'utilisateur afin de pouvoir l'afficher
+        local_user_theme = database.select('''SELECT theme FROM cantina_administration.user WHERE token= %s''',
+                                           (request.cookies.get('token')), 1)
+
+        # On récupère les permission de l'utilisateur afin de pouvoir afficher les options qui correspondent
+        local_user_permission = database.select('''SELECT * FROM cantina_administration.permission 
+                    WHERE user_token = %s''', (request.cookies.get('token')), 1)
+
         if request.method == 'POST':  # Si il fait une requete de type POST
             _create_user = create_user(database)  # Création de l'utilisateur
             return redirect(url_for('show_user', user_token=_create_user))
         elif request.method == 'GET':  # Si il fait une requete de type GET
-            return render_template('Administration/add_user.html')
+            return render_template('Administration/add_user.html', modules_info=modules_info, local_user_theme=local_user_theme, user_permission=local_user_permission)
 
     elif verify_login(database) == "desactivated":
         return redirect(url_for('sso_login', error='2'))
