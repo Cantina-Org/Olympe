1519c1c9061ce19249e40117e3bee4c4281f74c8
Matyu9 <59774749+Matyu9@users.noreply.github.com>
2025-01-22 09:06:31 +0100

    - fix Circular Import
    Took 10 minutes

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ from Cogs.Administration.User.global_permission import global_permission_cogs
 from Cogs.Administration.User.smtp_config import smtp_config_cogs
 from Cogs.Administration.User.smtp_test import smtp_test_cogs
 from Cogs.Administration.Modules.show_modules import show_modules_cogs
-from Cogs.Administration.Modules.maintenance import maintenance_cogs
 from Cogs.Administration.Modules.add_modules import add_modules_cogs
 
 from Cogs.Socket.heart_beat_cogs import heart_beat_cogs
@@ -151,6 +150,7 @@ def add_modules():
 
 @app.route('/admin/modules/maintenance/', methods=['POST'])
 def maintenance():
+    from Cogs.Administration.Modules.maintenance import maintenance_cogs
     return maintenance_cogs(database)
 
 
